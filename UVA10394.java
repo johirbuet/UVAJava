32f892d3764c3902e3a8230cd0d82a9ce8233f1b
johirbuet <johirbuet@gmail.com>
2018-03-11 17:03:15 -0500

    10394

--- UVA10394.java
+++ UVA10394.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class UVA10394 {
 	public static void main(String[] args) {
 		boolean [] prime = new boolean[20000001];
 		Arrays.fill(prime, true);
+		List<Pair> list = new ArrayList<>();
 		for(int i =2;i*i < 20000001;i++) {
 			if(prime[i]) {
 				for(int j = i*i;j< 20000001;j += i) {
@@ -12,20 +15,29 @@ public class UVA10394 {
 				}
 			}
 		}
+		for(int i =2;i<20000001 - 2;i++) {
+			if(prime[i] == true && prime[i+2] == true) {
+				list.add(new Pair(i, i+2));
+			}
+			if( i > 2) {
+				i++;
+			}
+		}
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNextInt()) {
 			int N = sc.nextInt();
-			int c = 0;
-			for(int i = 2;i < 20000001;i++) {
-				if(prime[i] && prime[i+2]) {
-					c++;
-					if(c == N) {
-						System.out.printf("(%d, %d)\n",i,i+2);
-						break;
-					}
-				}
-			}
+			Pair p = list.get(N - 1);
+			System.out.printf("(%d, %d)\n",p.x,p.y);
+
 		}
 		sc.close();
+	}
+	static class Pair{
+		int x;
+		int y;
+		public Pair(int x,int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 }
