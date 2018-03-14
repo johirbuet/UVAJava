5f1f460385744c060004ebfd4eb01e1e0b7c7dc6
johirbuet <johirbuet@gmail.com>
2018-03-14 00:13:23 -0500

    UVA441

---
+++ UVA441.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UVA441 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean firstcase = true;
+		while(sc.hasNextInt()) {
+			int N = sc.nextInt();
+			if( N == 0) break;
+			int [] A = new int[N];
+			for(int i =0; i< N; i++) {
+				A[i] = sc.nextInt();
+			}
+			List<List<Integer>> result = new ArrayList<>();
+			backtrack(A, 0, new ArrayList<>(), result);
+			if(!firstcase) {
+				System.out.println();
+			}else {
+				firstcase = false;
+			}
+			for(List<Integer> l : result) {
+				boolean first = true;
+				for(int i =0;i< l.size();i++) {
+					if(!first) {
+							System.out.print(" ");
+					}else {
+						first = false;
+					}
+					System.out.print(l.get(i));
+					
+				}
+				System.out.println();
+			}
+			//System.out.println();
+			}
+		sc.close();
+	}
+	
+	public static void backtrack(int [] A, int start,List<Integer> list, List<List<Integer>> result) {
+		if( start > A.length) return;
+		if(list.size() == 6) {
+			result.add(new ArrayList<>(list));
+			return;
+		}
+		for(int i = start; i< A.length;i++ ) {
+			if( i > start && A[i] == A[i-1]) continue;
+			list.add(A[i]);
+			backtrack(A, i+1, list, result);
+			list.remove(list.size() - 1);
+		}
+	}
+}
