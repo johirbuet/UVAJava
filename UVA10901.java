ad7cede548b0c5f9eb6bb0d1f3c3c5eff414a1fa
johirbuet <johirbuet@gmail.com>
2018-04-09 16:04:23 -0500

    10901

--- UVA10901.java
+++ UVA10901.java
@@ -3,6 +3,14 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class UVA10901 {
+	static class Car {
+		int id;
+		int t;
+		public Car(int id,int t) {
+			this.id = id;
+			this.t= t;
+		}
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = Integer.parseInt(sc.nextLine());
@@ -11,59 +19,80 @@ public class UVA10901 {
 			int n = Integer.parseInt(l[0]);
 			int t = Integer.parseInt(l[1]);
 			int m = Integer.parseInt(l[2]);
-			Queue<Integer> left = new LinkedList<>();
-			Queue<Integer> right = new LinkedList<>();
+			Queue<Car> left = new LinkedList<>();
+			Queue<Car> right = new LinkedList<>();
+			int maxt = 0;
 			for(int i = 0; i< m ; i++) {
 				String [] s = sc.nextLine().split(" ");
 				int a = Integer.parseInt(s[0]);
+				maxt = Math.max(a, maxt);
 				String b = s[1];
 				if(b.equals("left")) {
-					left.add(a);
+					left.add(new Car(i,a));
 				}else {
-					right.add(a);
+					right.add(new Car(i,a));
 				}
 			}
 			boolean lside = true;
-			int wtime = 0;
-			int count = 0;
-			while(!(left.isEmpty() && right.isEmpty())) {
+			int wt = 0;
+			int [] res = new int[m];
+			while(true) {
+				int count = 0;
+				if(left.size() == 0 && right.size() == 0) {
+					break;
+				}
+				if((left.size() > 0 && wt < left.peek().t)) {
+					if(right.size() > 0 && wt < right.peek().t) {
+						wt += Math.min(left.peek().t - wt, right.peek().t - wt);
+						continue;
+					}
+					else if (right.size() == 0) {
+						wt += left.peek().t - wt;
+						continue;
+					}
+				}
+				if((right.size() > 0 && wt < right.peek().t)) {
+					if(left.size() > 0 && wt < left.peek().t) {
+						wt += Math.min(left.peek().t - wt, right.peek().t - wt);
+						continue;
+					}
+					else if (left.size() == 0) {
+						wt += right.peek().t - wt;
+						continue;
+					}
+				}
 				if(lside) {
-					if(left.isEmpty()) {
-						wtime += t;
-						lside = false;
-						continue;
-					}
 					
-					while(!left.isEmpty() && left.peek() <= wtime && count <=n) {
-						left.poll();
-						count++;
+					if(left.size() > 0 && left.peek().t <= wt && count < n) {
+						
+						int temp = wt  + t;
+						while(left.size() > 0 && left.peek().t <= wt && count < n) {
+							res[left.poll().id] = temp;
+							//left.poll();
+							count++;
+						}
+						
 					}
-					wtime += t;
-					while(count > 0) {
-						System.out.println(wtime);
-						count--;
-					}
-					lside = false;
+					wt += t;
+					lside = !lside;
 				}else {
-					if(right.isEmpty()) {
-						wtime += t;
-						lside = true;
-						continue;
+					if(right.size() > 0 && right.peek().t <= wt) {
+						int temp = wt  + t;
+						while(right.size() > 0 && right.peek().t <= wt && count < n) {
+							//System.out.println(temp);
+							res[right.poll().id] = temp;
+							//right.poll();
+							count++;
+						}
 					}
-					
-					while(!right.isEmpty() && right.peek() <= wtime && count <= n) {
-						right.poll();
-						count++;
-					}
-					wtime += t;
-					while(count > 0) {
-						System.out.println(wtime);
-						count--;
-					}
-					lside = true;
+					wt += t;
+					lside = !lside;
 				}
 			}
-			System.out.println();
+			for(int i =0; i < m;i++) System.out.println(res[i]);
+			if(x < T) {
+				System.out.println();
+			}
 		}
 		sc.close();
 	}
